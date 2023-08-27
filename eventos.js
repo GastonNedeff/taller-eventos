d47b7a260e3b58425cf64bd7f8215b3fe13de41e
Gaston Nedeff <robertgastonnedeff@gmail.com>
2023-08-27 11:31:19 -0300

    Ahora si funciona bien

--- eventos.js
+++ eventos.js
@@ -1,13 +1,17 @@
-function mostrarAlerta() {
+function mostrarAlertaBoton() {
     alert("¡Hola!");
 }
 
-function mostrarAlertaDiv() {
-    alert("¡Hola! Soy el div");
+const botonSaludar = document.getElementById("saludarButton");
+
+botonSaludar.addEventListener("click", mostrarAlertaBoton);
+
+function mostrarAlertaDiv(event) {
+    if (event.target === divBotonContainer) {
+        alert("¡Hola! Soy el div");
+    }
 }
 
-
 const divBotonContainer = document.querySelector(".boton-container");
-
 
 divBotonContainer.addEventListener("click", mostrarAlertaDiv);
